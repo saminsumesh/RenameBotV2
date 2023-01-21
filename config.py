62980cb41bd15adc650b77b9e7c41d4046c2d6a1
Zac <91833744+saminsumesh@users.noreply.github.com>
2023-01-21 13:40:47 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-PREMUIM_USER = int(environ.get(5155880931))          
+# PREMUIM_USER = int("5155880931")       
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
